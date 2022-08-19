00ddf0214a2f4aa12b29f30ee553fd8c8326dd61
chris-hildebrandt <ch.hild15@gmail.com>
2022-08-18 23:52:45 -0600

    blog card up

--- src/services/BlogsService.js
+++ src/services/BlogsService.js
@@ -1,7 +1,14 @@
+import { Blog } from "../models/Blog.js";
+
 const { AppState } = require("../AppState.js");
 const { server } = require("./AxiosService.js");
 
 class BlogsService{
+
+  async getBlogs() {
+    const res = await server.get('/api/blogs')
+    AppState.blogs = res.data.map(b => new Blog(b))
+  }
 
   async getBlogsByCreatorI(creatorId){
     const res = await server.get('api/blogs', {
@@ -10,6 +17,8 @@ class BlogsService{
       }
     })
     AppState.profileBlogs = res.data.map()
-    
+
   }
 }
+
+export const blogsService = new BlogsService()
